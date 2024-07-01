e50b1d64b1db9557455bc5c361ff66d338960bdf
Georgemuchiri <georgw.muchiri@student.moringaschool.com>
2024-07-01 15:19:28 +0300

    Add my work

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet > 2000 && feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return "No can do.";
+  } else {
+    return "That will be twenty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+console.log(scuberGreetingForFeet(200))
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  return "Thank you so much"
 }
 
-function switchOnCharmFromTip(){
+
+
+
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-}
+  if (tip ==='generous'){
+  return "Thank you so much.";
+} else if (tip ==='not as generous')
+      return  "Thank you.";
+   else{
+    return "Bye.";
+  }}
